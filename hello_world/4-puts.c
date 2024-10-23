2d9dc6539cc57c55c37f76c692b516e8a0ed70c9
TechNovaExplorer <debanne.enzo@gmail.com>
2024-10-23 09:09:18 +0000

    changement

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry point of the program
+  *
+  *decription: le programme doit afficher "Programming is like building a multilingual puzzle
+  *
+  *Return: toujours 0 (succes)
+  */
+Main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return 0
+}
