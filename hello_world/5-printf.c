66c903c9ad9d356c5d835fc52b08a79d90e0cba2
TechNovaExplorer <debanne.enzo@gmail.com>
2024-10-23 09:31:30 +0000

    changement

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - point d'entrer du programme
+ *
+ * return : toujours 0 (succes)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
